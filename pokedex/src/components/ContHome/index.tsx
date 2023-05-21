6bc3969162c961d27b537314bad5875af77e642e
Anderson Daniel <anderbreiksondaniel@gmail.com>
2023-05-21 04:35:42 -0300

    home concluída (sem respon)

--- pokedex/src/components/ContHome/index.tsx
+++ pokedex/src/components/ContHome/index.tsx
@@ -7,7 +7,7 @@ export const ContHome = () => {
             <div className='column'>
                 <div className='info'>
                     <h1 className='title'>
-                        <strong>Find</strong> all your<br></br> 
+                        <strong>Find</strong> all your<br></br>
                         favorite<br></br> <strong>Pokemon</strong>
                     </h1>
                     <h2 className='sub'>
@@ -15,21 +15,21 @@ export const ContHome = () => {
                         its strengths, disadvantages and<br></br>
                         abilities
                     </h2>
-                    <button>
-                        See Pokemons
+                    <button className='btn'>
+                        See pokemons
                     </button>
                 </div>
                 <div>
                     <img src={Banner} className='banner' alt="" />
                 </div>
             </div>
-            <div>
-                <h2>
-                    Make with ❤ for the PokéSpartans team Platzi Master
-                </h2>
-                <h2>
+            <div className='footer'>
+                <h3>
+                    Make with &#128147; for the PokéSpartans team Platzi Master
+                </h3>
+                <h3>
                     Ours Team
-                </h2>
+                </h3>
             </div>
         </section>
     )
