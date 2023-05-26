bcff9107b2180a800a69b9bb77e65a4eac01d391
Anderson Daniel <anderbreiksondaniel@gmail.com>
2023-05-25 22:50:40 -0300

    erro netlyfi

--- pokedex/src/pages/Pokedex/index.tsx
+++ pokedex/src/pages/Pokedex/index.tsx
@@ -58,9 +58,9 @@ export const Pokedex = () => {
 
            
 
-            interface TypeProps {
-                types: PokemonType[];
-            }
+            // interface TypeProps {
+            //     types: PokemonType[];
+            // }
 
             setLoading(true);
 
